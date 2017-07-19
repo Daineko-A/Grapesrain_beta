9f02d34a6f6cd4bf8e5c78147c2a78154dc379bb
Alexandr Daineko <alexandr.daineko@gmail.com>
2017-07-19 15:20:02 +0300

    clear/

--- web/src/main/java/by/grapesrain/controller/UserController.java
+++ web/src/main/java/by/grapesrain/controller/UserController.java
@@ -2,10 +2,12 @@ package by.grapesrain.controller;
 
 import by.grapesrain.entitys.Departament;
 import by.grapesrain.entitys.User;
+import by.grapesrain.entitys.UserCard;
 import by.grapesrain.services.DepartamentService;
 import by.grapesrain.services.UserRoleService;
 import by.grapesrain.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.hibernate5.HibernateOptimisticLockingFailureException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -37,58 +39,10 @@ public class UserController {
     @ModelAttribute("userDetails")
     public User userDetails() {
         return userService.findByLogin(((UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getUsername());
-//        return null;
     }
-
-//    @ModelAttribute("userCard")
-//    public User userCard() {
-//        return userService.findByLogin(((UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getUsername());
-////        return null;
-//    }
 
     @GetMapping("/userdetails")
     public String userPage() {
         return "userdetails";
     }
-
-
-
-
-//    @ModelAttribute("allDepartaments")
-//    public List<Departament> allDepartaments() {
-//        return departamentService.allDepartaments();
-//    }
-//
-//    @ModelAttribute("userDetailsAdm")
-//    public User userDetailsAdm(@PathVariable("userId") Integer userId) {
-//        return userService.findById(Long.valueOf(userId));
-//    }
-//
-//    @ModelAttribute("userDetails")
-//    public User userDetails() {
-//        return userService.findByLogin();
-//    }
-//
-//    @ModelAttribute("user")
-//    public User user() {
-//        return new User();
-//    }
-//
-//    @GetMapping("/userdetails")
-//    public String userPage() {
-//        return "userdetails";
-//    }
-//
-//    @GetMapping("/userdetailsadm/{userId}")
-//    public String userPageAdmin(@RequestParam long userId) {
-//        return "userdetailsadm/{userId}";
-//    }
-//
-//    @PostMapping("/userdetailsadm/{userId}")
-//    public String registerUser(User user, Model model, @RequestParam long userId) {
-////        user.setDepartament(departamentService.findById(departament));
-////        user.setPassword(passwordEncoder.encode(user.getPassword()));
-////        userService.save(user);
-//        return "userdetails";
-//    }
 }
